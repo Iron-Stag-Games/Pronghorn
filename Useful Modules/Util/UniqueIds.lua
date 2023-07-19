81efe0d28f222d82d111acd5d396d0475aaea128
hoontee <hunter.chambers@gmail.com>
2023-07-19 02:38:09 -0500

    Update UniqueIds.lua

--- Useful Modules/Util/UniqueIds.lua
+++ Useful Modules/Util/UniqueIds.lua
@@ -12,53 +12,56 @@ local UniqueIds = {}
 -- Helper Variables
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-type UniqueIds = {
-	UnusedIds: {string};
-	NumUniqueIds: number;
-	NumUnusedIds: number;
-	Bytes: number;
+export type UniqueIds = {
 	GetNewId: (UniqueIds) -> (string);
-	FreeId: (UniqueIds, string) -> ();
+	FreeId: (UniqueIds, string) -> (boolean);
 }
-
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Helper Functions
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-local function getNewId(self: UniqueIds): string
-	if self.NumUnusedIds > 0 then
-		local id = table.remove(self.UnusedIds, self.NumUnusedIds) :: string
-		self.NumUnusedIds -= 1
-		return id
-	else
-		local id = string.pack("I" .. self.Bytes, self.NumUniqueIds)
-		self.NumUniqueIds += 1
-		return id
-	end
-end
-
-local function freeId(self: UniqueIds, id: string): boolean
-	if id:byte() + 1 <= self.NumUniqueIds then
-		table.insert(self.UnusedIds, id)
-		self.NumUnusedIds += 1
-		return true
-	end
-	return false
-end
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Module Functions
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --- @ignore
-function UniqueIds.new(bytes: number): UniqueIds
+function UniqueIds.new(bytes: number, idsInUse: {string}?): UniqueIds
+	local unusedIds = {};
+	local numUniqueIds = 0;
+	local numUnusedIds = 0;
+
+	if idsInUse then
+		for _, id in idsInUse do
+			numUniqueIds = math.max(numUniqueIds, id:byte() + 1)
+		end
+
+		for index = 1, numUniqueIds do
+			local id = string.char(index - 1)
+			if not table.find(idsInUse, id) then
+				table.insert(unusedIds, id)
+				numUnusedIds += 1
+			end
+		end
+	end
+
 	return {
-		UnusedIds = {};
-		NumUniqueIds = 0;
-		NumUnusedIds = 0;
-		Bytes = bytes;
-		GetNewId = getNewId;
-		FreeId = freeId;
+		GetNewId = function(_self: UniqueIds): string
+			if numUnusedIds > 0 then
+				local id = table.remove(unusedIds, numUnusedIds) :: string
+				numUnusedIds -= 1
+				return id
+			else
+				local id = string.pack("I" .. bytes, numUniqueIds)
+				numUniqueIds += 1
+				return id
+			end
+		end;
+
+		FreeId = function(_self: UniqueIds, id: string): boolean
+			if id:byte() + 1 <= numUniqueIds then
+				table.insert(unusedIds, id)
+				numUnusedIds += 1
+				return true
+			end
+			return false
+		end;
 	}
 end
 
